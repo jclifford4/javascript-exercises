7a81336d9e4b0644373609939a5e44a086908d54
joseph <92230691+jclifford4@users.noreply.github.com>
2023-02-21 00:56:13 -0800

    Test 4 Passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) 
+{
+    return array.filter
+                (element => !args.includes(element))
 };
 
 // Do not edit below this line
