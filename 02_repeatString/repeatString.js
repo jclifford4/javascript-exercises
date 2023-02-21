cb569629e6566c446d129c664f30b26c62aab2bd
joseph <92230691+jclifford4@users.noreply.github.com>
2023-02-21 00:17:15 -0800

    Test for 1,2 Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) 
+{
 
+    if (num === 0 || string === '')
+        return '';
+    
+    if (num < 0)
+        return 'ERROR';
+
+    result = string;
+    for (let i = 1; i < num; i++)
+    {
+        result += string;
+
+    }
+
+    return result;
 };
 
 // Do not edit below this line
