857e444a72fec2a68fbdd13a38a10312c4c0fc18
joseph <92230691+jclifford4@users.noreply.github.com>
2023-02-21 02:28:12 -0800

    Test 7 Passes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) 
+{
+  let result = (5 / 9) * (fahrenheitTemperature - 32);
+
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) 
+{
+  let result = (celsiusTemperature * (9 / 5)) + 32;
+
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
