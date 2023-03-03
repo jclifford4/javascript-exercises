81c0a5b4af9dfa55e02a3c26103aff15d084b1fb
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-03-03 02:01:31 -0800

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanString = string.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const reversedString = cleanString.split('').reverse().join('');
+
+    return cleanString === reversedString;
 
 };
 
