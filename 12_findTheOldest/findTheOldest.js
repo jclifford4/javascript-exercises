f7e98dcde80a463a76e53f92b42603cf74c575bd
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-03-03 19:16:15 -0800

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) 
+{
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
+
+
+function getAge(birth, death)
+{
+    const dateNow = new Date().getFullYear();
+
+    if (!death)
+        return dateNow - birth;
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
