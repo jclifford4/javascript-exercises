71af3b74e5af5201a4d4f778947c8ed30567ae86
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-03-03 18:36:09 -0800

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (typeof(arguments) === String || x < 0)
+        return "OOPS";
+        
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < x; i++)
+    {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
